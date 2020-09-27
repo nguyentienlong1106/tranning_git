8b645b4f61706f4acef5c87e1d9bec61729cbd3f
nguyentienlong1106 <tienlong1106@gmail.com>
2020-09-27 22:23:15 +0300

    The second lap

--- script.bash
+++ script.bash
@@ -1,2 +1,13 @@
-echo "Welcome to ITMO"
-
+#!/bin/bash
+x=$1
+y=$2
+if [ $x -gt -100 -a $x -lt 100 -a $y -gt -100 -a $y -lt 100 ]; then
+	if [[ $y -eq 0 ]]; then
+		echo $(($x+$y)) $(($x-$y)) $(($x*$y)) '#'
+	else
+		echo $(($x+$y)) $(($x-$y)) $(($x*$y)) $(bc <<< "scale=2; $x/$y")
+	fi
+else
+	echo 'you have misstake'
+fi
+exit 0
